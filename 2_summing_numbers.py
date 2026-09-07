e98c036dbee86ee3cc574f039c6ace6aa0dbc3fa
Chloe <chloehampton_dev@outlook.com>
2026-09-07 15:14:54 +1000

    Write pseudocode

---
+++ 2_summing_numbers.py
@@ -0,0 +1,11 @@
+# 2. Summing Numbers
+"""
+Pseudocode:
+function mysum(*numbers):
+    result = 0
+    repeat numbers of times:
+        result = result + numbers
+    return result
+
+display sum
+"""
