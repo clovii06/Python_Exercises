88b759381810ea4905ae1149eb7666a78f94bb30
Chloe <chloehampton_dev@outlook.com>
2026-09-07 13:03:01 +1000

    Write pseudocode for guessing_game function

---
+++ 1_number_guessing_game.py
@@ -0,0 +1,15 @@
+# 1. Number Guessing Game
+"""
+Pseudocode:
+function guessing_game():
+    answer = random number
+    while True:
+        get guess
+        if guess < answer:
+            display higher
+        else if guess > answer:
+            display lower
+        else:
+            display correct
+            break
+"""
