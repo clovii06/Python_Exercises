f6e2cccb518c33058bd2327625b16177b4ef8493
Chloe <chloehampton_dev@outlook.com>
2026-09-07 13:11:09 +1000

    Clean up code

--- 1_number_guessing_game.py
+++ 1_number_guessing_game.py
@@ -17,12 +17,16 @@ import random
 
 def guessing_game():
         answer = random.randint(1, 100)
+
         while True:
             guess = int(input("Guess a number between 1 and 100: "))
+
             if guess < answer:
                 print("Higher!")
+
             elif guess > answer:
                 print("Lower!")
+
             else:
                 print(f"You got it! The answer was {answer}.")
                 break
