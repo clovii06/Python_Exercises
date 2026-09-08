86e4cb746e94cc86ff21011a152880ad0a5319d4
Chloe <chloehampton_dev@outlook.com>
2026-09-08 11:46:14 +1000

    Fix logic of pseudocode

--- 3_run_timing.py
+++ 3_run_timing.py
@@ -5,10 +5,16 @@ Pseudocode:
 function run_timing():
     total_run_time = 0
     number_of_runs = 0
-    while run_time != "":
+
+    while True:
         get run_time
-        total_run_time = run_time + total_run_time
-        number_of_runs + 1
+        total_run_time += run_time
+        number_of_runs += 1
+        get run_time
+
+        if not run_time:
+            break
+
     average_run_time = total_run_time / number_of runs
     display average
 """
