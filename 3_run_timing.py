21118ba831924b2d1d39a5b949acde1ab2679d9c
Chloe <chloehampton_dev@outlook.com>
2026-09-08 11:52:17 +1000

    Edit pseudocode layout and write run_timing() function

--- 3_run_timing.py
+++ 3_run_timing.py
@@ -7,14 +7,31 @@ function run_timing():
     number_of_runs = 0
 
     while True:
+        if not run_time:
+            break
+
         get run_time
         total_run_time += run_time
         number_of_runs += 1
-        get run_time
-
-        if not run_time:
-            break
 
     average_run_time = total_run_time / number_of runs
     display average
 """
+
+def run_timing():
+    total_run_time = 0
+    number_of_runs = 0
+
+    while True:
+        run_time = float(input("Enter run time: "))
+
+        if not run_time:
+            break
+
+        total_run_time += run_time
+        number_of_runs += 1
+
+    average_run_time = total_run_time / number_of_runs
+    print(f"Average run time: {average_run_time}")
+
+run_timing()
