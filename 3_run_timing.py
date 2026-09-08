1f06f553b2dfade148f80a5a888d5ae4df0ce2c0
Chloe <chloehampton_dev@outlook.com>
2026-09-08 11:25:12 +1000

    Write pseudocode for run_timing function, work out logic

---
+++ 3_run_timing.py
@@ -0,0 +1,14 @@
+# 3. Run Timing
+
+"""
+Pseudocode:
+function run_timing():
+    total_run_time = 0
+    number_of_runs = 0
+    while run_time != "":
+        get run_time
+        total_run_time = run_time + total_run_time
+        number_of_runs + 1
+    average_run_time = total_run_time / number_of runs
+    display average
+"""
