20d611503c63b668fc3de9a84a60a3410bea2804
Chloe <chloehampton_dev@outlook.com>
2026-09-09 23:05:34 +1000

    Add 4_hexadecimal_output.py and start pseudocode

---
+++ 4_hexadecimal_output.py
@@ -0,0 +1,16 @@
+# 4. Hexadecimal Output
+
+"""
+Pseudocode:
+
+function hex_output():
+    get hex_number
+
+    for index, place in enumerate(hex_number):
+        print(index, place)
+
+    result = (place[0] x 16) + (place[1] x 1)
+    place[1] x 1
+    display result
+
+"""
