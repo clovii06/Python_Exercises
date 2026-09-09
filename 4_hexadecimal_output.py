1a4ab3ece5ef9dfa9319e418b7965d2cbba260e1
Chloe <chloehampton_dev@outlook.com>
2026-09-09 23:45:30 +1000

    Clean up code and update pseudocode

--- 4_hexadecimal_output.py
+++ 4_hexadecimal_output.py
@@ -4,26 +4,23 @@
 Pseudocode:
 
 function hex_output():
+    decimal_number = 0
     get hex_number
 
     for power, digit in enumerate(reversed(hex_number)):
+        decimal_number = digit * (16 ** power)
+    display decimal_number
 
-
+hex_output()
 """
 
 def hex_output():
+    """Convert a hexadecimal number to a decimal"""
+    decimal_number = 0
     hex_number = input("Enter a hexadecimal number: ")
 
     for power, digit in enumerate(reversed(hex_number)):
-
-        digit = int(digit, 16) # Convert each digit to integer with base of 16
-        power = int(power) # Convert power to int
-
-        decimal_number = digit * (16 ** power)
-        print(decimal_number)
-
-
-
-
+        decimal_number = int(digit, 16) * (16 ** int(power))
+    print(decimal_number)
 
 hex_output()
