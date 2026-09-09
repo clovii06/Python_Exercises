26611a3f35a1ea16f633c30580ce71b43d050d2a
Chloe <chloehampton_dev@outlook.com>
2026-09-09 23:15:20 +1000

    Change variable names, start writing hex_output function

--- 4_hexadecimal_output.py
+++ 4_hexadecimal_output.py
@@ -6,11 +6,15 @@ Pseudocode:
 function hex_output():
     get hex_number
 
-    for index, place in enumerate(hex_number):
-        print(index, place)
-
-    result = (place[0] x 16) + (place[1] x 1)
-    place[1] x 1
-    display result
+    for power, digit in enumerate(hex_number):
+        print(power, digit)
 
 """
+
+def hex_output():
+    hex_number = input("Enter a hexadecimal number: ")
+
+    for power, digit in enumerate(hex_number):
+        print(f"Power: {power} Digit: {digit}")
+
+hex_output()
