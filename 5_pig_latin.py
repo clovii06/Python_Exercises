5b1045d5c0fb3e028bd6eed8bef8c4f31631c255
Chloe <chloehampton_dev@outlook.com>
2026-09-10 15:19:11 +1000

    Write rough pseudocode

--- 5_pig_latin.py
+++ 5_pig_latin.py
@@ -1,5 +1,17 @@
 # 5. Pig Latin
 """
 Pseudocode:
-...
+
+get english_word
+
+if first letter = vowel:
+    add "way"
+
+else:
+    slice first letter
+    move first letter to end
+    add "ay"
+
+display pig_latin_word
+
 """
