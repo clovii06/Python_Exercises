fda418e72ff488f1eb18d3b30a72c8c37166c134
Chloe <chloehampton_dev@outlook.com>
2026-09-10 15:58:13 +1000

    Update pseudocode and add comments

--- 5_pig_latin.py
+++ 5_pig_latin.py
@@ -2,24 +2,28 @@
 """
 Pseudocode:
 
-get english_word
+function pig_latin():
+    get english_word
 
-if english_word[0] = vowel:
-    add "way"
-
+    if english_word[0] in 'aeiou':
+        pig_latin_word = english_word + "way"
 else:
     slice first letter
     move first letter to end
-    add "ay"
+    pig_latin_word = english_word + "ay"
 
 display pig_latin_word
 
 """
 def pig_latin():
+    """Translate english word to pig latin"""
     english_word = input("Word: ")
 
+    # If first letter is a vowel
     if english_word[0] in 'aeiou':
         pig_latin_word = english_word + "way"
+
+    # If first letter is a consonant
     else:
         pig_latin_word = english_word[1:] + english_word[0] + "ay"
 
